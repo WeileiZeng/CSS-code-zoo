b9adc0ffcc222ba6d2eb359bac8e03721b91aea7
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-11-02 09:36:33 +0800

    finish simulation set up

--- generate_css_code.cpp
+++ generate_css_code.cpp
@@ -1,27 +1,63 @@
 #include "weilei_lib/weilei_lib.h"  //general include goes to weilei_lib_h
 #include "json.hpp"
 using json=nlohmann::json;
+#include <chrono> //print computation time
+#include <ctime>  
 //#include <ctime> //to get current time
 //using namespace common;
 
-int generate_css_code();
+//int generate_css_code();
+int generate_css_code(int n, int Gx_row, int Gz_row);
 
 int main(){
   std::cout<<" --------------------- begin generating CSS codes"<<std::endl;
-  generate_css_code();
+  //  generate_css_code();
+
+  //set up simulation
+  for (int n=12;n<21;n++){
+    for ( int Gx_row = 5;Gx_row<n-1;Gx_row++){
+      for ( int Gz_row = 5; Gz_row < n-Gx_row-1 && Gz_row < Gx_row +3 ; Gz_row ++){
+	std::cout<<"n="<<n<<", Gx_row="<<Gx_row<<", Gz_row="<<Gz_row<<std::endl;
+
+	auto start = std::chrono::system_clock::now();
+	std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+	std::cout <<".-.-.-.-.-.-.-.-.-.-.-."
+		  << "start computation at " << std::ctime(&start_time)<<std::endl;
+
+	generate_css_code(n,Gx_row,Gz_row);
+	//When G_x_row or Gz_row is too small (like 1), error occurs, probably get zero matrix somewhere
+
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end-start;
+	std::cout << "elapsed time: " << elapsed_seconds.count() << "s"
+		  << std::endl;
+      }
+    }
+  }
+  /*simulation log
+    run: n=8..13, Gx_row and Gz_row = 2..n-1
+    n=21..30
+    n=12..19
+   */
+  
   std::cout<<" --------------------- finish generating CSS codes"<<std::endl;
   return 0;
 }
 
-
-int generate_css_code(){
-
-  int seed = seed+common::get_time(3);
+/*parameters to set up:
+  num_cores,32
+  num_trials, 10000
+  how many instances to save for each parameter set, 3
+  data folder, ../data/CSS-Codes/run1
+ */
+int generate_css_code(int n, int Gx_row, int Gz_row){
+  //int generate_css_code(){
+  int seed = common::get_time(3)+100;//+seed;
   itpp::RNG_reset(seed);
   itpp::RNG_randomize();
 
-  int num_cores = 16; //32
-  int num_trials = num_cores * 10000;
+  int num_cores = 32; //32
+  int num_trials = num_cores * 10;
   int dx_max=0, dz_max=0;
   //#pragma omp parallel for num_threads(4)
 #pragma omp parallel for schedule(guided) num_threads(num_cores)
@@ -30,17 +66,21 @@ int generate_css_code(){
     CSSCode codeR;
     codeR.title="random code";
     // good choice: 15,6,6;
-    codeR.n=22;
-    codeR.Gx_row=15;
-    codeR.Gz_row=5;
+    codeR.n=n;//22;
+    codeR.Gx_row=Gx_row;//15;
+    codeR.Gz_row=Gz_row;//5;
 
     codeR.getGoodCode(0);//0 for disabling debug
     codeR.dist();
     /* if ( not codeR.is_valid() ){
       std::cout<<"The code is not valid"<<std::endl;
     }else{
-      codeR.dist();
-      }*/
+       //       try{
+       codeR.dist();
+	 //       }catch(std::exception e){
+	 //	 std::cout<<"The code is not valid. probably zero matrix"<<std::endl;
+	 //       }
+	 }*/
     //the following part should be in critical in the beginning, after some run it is okay to remove
 //#pragma omp critical
     {
@@ -91,7 +131,26 @@ int generate_css_code(){
 	//	std::cout<<filename_Gz<<std::endl;
 	GF2mat_to_MM(codeR.Gz,filename_Gx);
 	GF2mat_to_MM(codeR.Gz,filename_Gz);
-	//	GF2mat_to_MM(codeR.Gz,filename_
+
+	//save parameters into json file
+	json::object_t object_value = {
+	  {"n",codeR.n},
+	  {"k",codeR.k},
+	  {"d",d},
+	  {"Gx_row",Gx_row_rank},
+	  {"Gz_row",Gz_row_rank},
+	  {"dx",codeR.dx},
+	  {"dz",codeR.dz},
+	  {"index",j},
+	  {"filename_Gx",filename_Gx},
+	  {"filename_Gz",filename_Gz},
+	};
+	json j_object_t(object_value);
+	//	std::cout<<j_object_t<<std::endl;
+	std::ofstream filejson(filename_json);
+	filejson << j_object_t;
+	filejson.close();
+
 	std::cout<<"saved code to "<<filename_json<<std::endl;
 	break;
       }else{
@@ -104,7 +163,7 @@ int generate_css_code(){
     //    std::cout<<codeR.Gx<<std::endl;
     //std::cout<<"run"<<i<<", dx="<<codeR.dx<<std::endl;
   }
-  std::cout<<"dx_max = "<<dx_max<<", dz_max = "<<dz_max<<std::endl;
+  //  std::cout<<"dx_max = "<<dx_max<<", dz_max = "<<dz_max<<std::endl;
   
   //  codeR.set_up_CxCz(); // no need to do it. already genrated in GetRandomCode().
   
