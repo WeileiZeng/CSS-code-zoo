690b5103dc29632f508719a338ba52c56b850661
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-11-15 11:35:45 +0800

    add script to compile filename list

---
+++ get-filelist.sh
@@ -0,0 +1,21 @@
+# copy all json files to another folder. not in use any more
+date
+
+trial=run1
+folder=../data/CSS-Codes/${trial}
+
+#ls ${folder} |wc -l
+echo start generating json file list for folder ${folder}
+echo this may take ~1 minute
+
+find ${folder} -name "*json" | cut -c 24- > filelist-${trial}.txt 
+
+echo "number of files: `wc -l filelist-${trial}.txt`" 
+
+date
+echo finish
+
+
+# Sample run
+# run1 contains 214948 files with 71647 json files 
+# takes 30 secs to generate the list
