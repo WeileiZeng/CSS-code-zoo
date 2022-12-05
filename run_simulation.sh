440aea3d0e6c367fbdf3a22880d6035a4c6a860a
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-12-05 16:04:15 +0800

    add two curve for k=2

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,35 +1,51 @@
 make simulation
 
-# time ./simulation.out num_cores=32 debug=1 e_try=1000 output=tmp.json note="I am happy"
 
-code=n8k1d3-x4z3dx3dz3-0
-code=n7k1d3-x3z3dx3dz3-0
-output=steane2.json
-
-
-code=n11k1d3-x5z5dx3dz3-0
-code=n12k1d3-x6z5dx3dz3-9
-
-
-
-case 3 in 
-    3)
+case 25 in 
+# check k=1
+    13)
 	code=n13k1d3-x6z6dx3dz3-9
-	output=d3.json
 	;;
-    4)
+    14)
 	code=n15k1d4-x7z7dx4dz4-0
-	output=d4.json
 	;;
-    5)
+    15)
 	code=n23k1d5-x11z11dx5dz5-0
-	output=d5.json
 	;;
-
+#now for k=2, choose min n for that k,d
+    23)
+# for 0,1,2, the distance is not 3. need to check
+# also need to fix Gx Gz to be full rank
+#	code=n11k2d3-x4z5dx3dz3-0,1,3,
+	code=n11k2d3-x4z5dx3dz3-2
+	;;
+    24)
+	code=n18k2d4-x8z8dx4dz4-4
+#	code=n17k2d4-x7z8dx4dz4-0 #d=3
+	;;
+    25)
+	#can not find code with d=5, k=2
+	code=n26k2d5-x12z12dx6dz5-0
+	;;
 esac
 
-./simulation.out code_prefix=../data/CSS-Codes/run1/${code} \
-output=result/${output} num_cores=16 e_try=100000 debug=0
+output=${code}
 
+mode=0  #0 for check; 1 for steane; 2 for simulating from file
+
+date 
+srun --time=24:00:00 ./simulation.out \
+    note=${code} \
+    code_prefix=../data/CSS-Codes/run1/${code} \
+    output=result/${output}.json \
+    num_cores=32 \
+    e_try=10000 \
+    debug=0 \
+    mode=${mode}
 
 date
+
+# e_try=1,000,000 to get smooth curve
+# e_try=10,000 for quick result
+
+
