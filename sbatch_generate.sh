72882e106df30dbc48b2720fc7f11b9031a816a9
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-12-06 15:17:50 +0800

    clean code

--- sbatch_generate.sh
+++ sbatch_generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# check locally: bash sbatch_generate.sh
 
 #SBATCH --job-name=generate_array
 #SBATCH --mail-type=ALL
@@ -14,10 +15,10 @@ echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
 
-n_start=$SLURM_ARRAY_TASK_ID
-(( n_end = $SLURM_ARRAY_TASK_ID  + 1 ))
+#n_start=$SLURM_ARRAY_TASK_ID
+#(( n_end = $SLURM_ARRAY_TASK_ID  + 1 ))
 
 srun ./generate_css_code.out num_cores=16 \
     code_folder="../data/CSS-Codes/run2/" \
-    n_start=$n_start \
-    n_end=$n_end
+    n_start="$((SLURM_ARRAY_TASK_ID))" \
+    n_end="$((SLURM_ARRAY_TASK_ID+1))"
