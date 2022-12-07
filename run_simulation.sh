cc069d5f5e0d7e69bb299dbd5e182f25c72a00a4
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-12-07 16:19:56 +0800

    finish trial simulation for run 2, k=1,2, d=3,4,5

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,45 +1,53 @@
 make simulation
 
-
-case 25 in 
+# run case by case
+case 23 in 
 # check k=1
     13)
-	code=n13k1d3-x6z6dx3dz3-9
+	code=n13k1d3-x6z6dx3dz3-9	
+	code=n7k1d3-x3z3dx3dz3-0	
 	;;
     14)
-	code=n15k1d4-x7z7dx4dz4-0
+	code=n15k1d4-x8z6dx4dz4-0
 	;;
     15)
-	code=n23k1d5-x11z11dx5dz5-0
+#	code=n23k1d5-x11z11dx5dz5-0
+	code=n25k1d5-x13z11dx5dz5-0  #run2
 	;;
+#    16)
+#	code=n30k1d6-x15z14dx6dz6-0
+#	;;
 #now for k=2, choose min n for that k,d
     23)
-# for 0,1,2, the distance is not 3. need to check
-# also need to fix Gx Gz to be full rank
-#	code=n11k2d3-x4z5dx3dz3-0,1,3,
-	code=n11k2d3-x4z5dx3dz3-2
+	code=n11k2d3-x4z5dx3dz3-0
 	;;
     24)
-	code=n18k2d4-x8z8dx4dz4-4
-#	code=n17k2d4-x7z8dx4dz4-0 #d=3
+	code=n18k2d4-x8z8dx4dz4-0
 	;;
     25)
-	#can not find code with d=5, k=2
-	code=n26k2d5-x12z12dx6dz5-0
+	code=n27k2d5-x13z12dx5dz5-0
 	;;
 esac
 
+
+
+#code=steane1
 output=${code}
 
-mode=0  #0 for check; 1 for steane; 2 for simulating from file
+mode=2  #0 for check; 1 for steane; 2 for simulating from file
+
+#    code_prefix=../data/CSS-Codes/run2/n21k2d4-x10z9dx4dz4-5 \
+#    code_prefix=../data/CSS-Codes/run1/${code} \
+#   code_prefix=../data/CSS-Codes/run2/n24k1d5-x13z10dx5dz5-0 \
 
 date 
-srun --time=24:00:00 ./simulation.out \
+srun --time=6:00:00 -p small -n 1 --cpus-per-task=16 \
+    ./simulation.out \
     note=${code} \
-    code_prefix=../data/CSS-Codes/run1/${code} \
+    code_prefix=../data/CSS-Codes/run2/${code} \
     output=result/${output}.json \
-    num_cores=32 \
-    e_try=10000 \
+    num_cores=16 \
+    e_try=100000 \
     debug=0 \
     mode=${mode}
 
