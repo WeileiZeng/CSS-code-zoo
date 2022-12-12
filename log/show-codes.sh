f69a255f0806de7a6cf0b232bc74db34b14d1ae3
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-12-12 17:24:10 +0800

    polish cmd

--- log/show-codes.sh
+++ log/show-codes.sh
@@ -1,2 +1,2 @@
-echo "this display the last lines in log file"
+echo "this display the last lines in log file, and one can check number of codes saved in each run."
 for i in gen*; do tail -n 3 $i |head -n 1; done
