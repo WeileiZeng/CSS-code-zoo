f214c3acb7db04f1d4d9d3ad9bf7846656ec2180
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2022-12-19 16:24:07 +0800

    add QOS options: expedite/short

--- sbatch_generate.sh
+++ sbatch_generate.sh
@@ -5,13 +5,17 @@
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=16
-#SBATCH --time=06:00:00
-#SBATCH --array=5-10%1
+#SBATCH --time=6:00:00
+#SBATCH --array=4-30%1
 #SBATCH --partition=small
+#SBATCH --qos=expedite #expedite 6 hours, short 2 hours
 #SBATCH --output=log/%x_%A_%a.log
-#SBATCH --nice=1000 #larger number with smaller priority
+ #SBATCH --error=log/%x_%A_%a.err
+ #SBATCH --nice=10 #larger number with smaller priority
  #SBATCH --begin=23:00:00
  #SBATCH --test-only
+
+# echo "n=30, k=1,2,3"
 
 echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
@@ -26,12 +30,13 @@ date
 #n_start=$SLURM_ARRAY_TASK_ID
 #(( n_end = $SLURM_ARRAY_TASK_ID  + 1 ))
 (( num_cores = 16 ))
-
+(( n_start = $SLURM_ARRAY_TASK_ID ))
+#(( n_start = 30 ))
 srun ./generate_css_code.out num_cores=$num_cores \
     code_folder="../data/CSS-Codes/run2/" \
-    n_start="$((SLURM_ARRAY_TASK_ID))" \
-    n_end="$((SLURM_ARRAY_TASK_ID+1))" \
-    num_trials="$((num_cores*10))" \
+    n_start="$((n_start))" \
+    n_end="$((n_start+1))" \
+    num_trials="$((num_cores*100))" \
     note="node-${SLURMD_NODENAME}-proc-${SLURM_PROCID}"
 
 date
