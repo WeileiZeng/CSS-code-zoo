25efd4e5391853059b2eb635172c2b0526f61c44
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2023-01-05 19:53:27 +0800

    clean code; remove recover function

--- processing-codes.py
+++ processing-codes.py
@@ -31,10 +31,11 @@ class Database():
 def dot_row(row):
     s=''
     for i in str(row):
+        #replace 0 and , for better display
         if i == '0' or i == ',':
             s += ' '
-        elif i == '9':
-            s += '.'
+#        elif i == '9':
+#            s += '.'
         else:
             s += i
     return s
@@ -112,7 +113,7 @@ def main():
 #                        filename=data_folder+'/'+ line[:-7]+str(9-i)+'.json'
                         if os.path.isfile(filename1):
 #                            print(filename1, 'is file')
-                            max_distance_table_index[n][k] = 9-i
+                            max_distance_table_index[n][k] = (9-i+1) %10
                             break
 
         except:
@@ -135,6 +136,7 @@ def main():
     print(["total number of codes:", num_of_codes])
     print(datetime.datetime.now())
     with open(log_file,'a') as f:
+        f.write("total number of codes: {}".format(num_of_codes))
         f.write(string)
 
 
@@ -147,74 +149,7 @@ def json_test():
             if d > max_distance_table[n][k]:
                 max_distance_table[n][k] = d
 
-#test()
-#exit()
-
-#retrieve trashed files. hopefully not in need anymore
-def recover():
-    data_folder = '../data/CSS-Codes/run2'
-    trash_folder = '../data/CSS-Codes/trash'
-#    filename_list = '../data/CSS-Codes/filelist-run1.txt'
-    filename_list = 'trashed-filelist-run2.txt'  #./get-filelist.sh
-    log_file = 'run.log'
-
-    recover_folder='../data/CSS-Codes/recover'
-    print('data_folder:\t'+data_folder)
-    print('trash_folder:\t'+trash_folder)
-    print('filename_list:\t'+filename_list)
-    print('log_file:\t'+log_file)
-    print(datetime.datetime.now())
-
-    num_of_codes=0   
-    n,k,d=-1,-1,-1
-    #save max distance for each (n,k)
-    max_distance_table=[[ 0 for k in range(28)] for n in range(31) ]
-    max_distance_table[0]=[_ % 10 for _ in range(28)]
-
-    #save index for each (n,k,d), to see if the database is complete
-    max_distance_table_index=[[ 0 for k in range(28)] for n in range(31) ]
-    max_distance_table_index[0]=[_ % 10 for _ in range(28)]
-    # d=table[n][k]
-
-    for line in fileinput.input(filename_list):
-        num_of_codes += 1
-        if (num_of_codes % 10000 == 0):
-            print('finish processing '+str(num_of_codes)+' codes')
-        try:
-            filename=trash_folder+'/'+ line[:-1]
- #           print(trash_folder+'/'+ line[:-1])
-# 
-
-            if os.path.isfile(filename):
-                os.system('mv '+ trash_folder+'/'+ line[:-1]+' ' +trash_folder+'/'+ line[:-6]+'Gx.mm '+trash_folder+'/'+ line[:-6]+'Gz.mm ' +recover_folder)
-#            else:
-#                print(filename,'no such file')
-#                break
-
-
-
-                    
-        except:
-            print('invalid json: '+line)
-            # move it to trash
-            # os.system('mv '+ data_folder+'/'+ line[:-1]+' ' +data_folder+'/'+ line[:-6]+'Gx.mm '+data_folder+'/'+ line[:-6]+'Gz.mm ' +trash_folder)
-            # print(line+' moved to trash')
-
-    exit()
-    print('Completeness table, .=9')
-    string = pretty_2D_list(max_distance_table_index)    
-    with open(log_file,'a') as f:
-        f.write(string)
-
-    print('[n,k,d] table')
-    string = pretty_2D_list(max_distance_table)
-    print(["total number of codes:", num_of_codes])
-    print(datetime.datetime.now())
-    with open(log_file,'a') as f:
-        f.write(string)
-
-
-def test():
+def database_test():
     db=Database()
     data_folder = '../data/CSS-Codes/run3'
     db.create(data_folder)
