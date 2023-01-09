4b86c81e4b353536c07dfdf3760f01b0d229381a
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2023-01-09 17:01:11 +0800

    add density info

--- verification.cpp
+++ verification.cpp
@@ -12,6 +12,7 @@ bool verify(std::string code_prefix){
 	CSSCode code;
 	code.load(code_prefix);
 	code.dist();
+	code.Gx.density();
 	std::ifstream jsonfile(code_prefix+".json");
 	json data = json::parse(jsonfile);
 	jsonfile.close();
@@ -21,6 +22,23 @@ bool verify(std::string code_prefix){
 	//	std::cout<<"caculated distance d="<<code.d<<std::endl;
 
 }
+
+bool verify(std::string code_prefix, double & rho_x, double & rho_z, int & n_Gx, int & n_Gz){
+	CSSCode code;
+	code.load(code_prefix);
+
+	rho_x = code.Gx.density();
+	rho_z = code.Gz.density();
+	n_Gx = code.Gx.rows()*code.Gx.cols();
+	n_Gz = code.Gz.rows()*code.Gz.cols();
+
+	code.dist();
+	std::ifstream jsonfile(code_prefix+".json");
+	json data = json::parse(jsonfile);
+	jsonfile.close();
+	return code.d == data["d"];		
+}
+
 
 /* Counting number of lines in a file. same as `wc -l filename`
  */
@@ -54,9 +72,12 @@ int main(int args, char ** argv){
 
     //get number of codes in advance to set up parallel for
     const int code_total=count_lines(filename_list);
+    //    const int code_total=1000;
     std::cout<<"Number of codes: "<<code_total<<std::endl;
     //std::cout<<"No printing on srun due to slow network"<<std::endl; //fixed by setting srun --unbuffered
     int code_count=0,code_mistake=0;
+    double rho_x_total=0,rho_z_total=0;
+    int n_Gx_total=0,n_Gz_total=0;
     std::ifstream file(filename_list);
     if (file.is_open()) {
 #pragma omp parallel for schedule(guided) num_threads(num_cores)
@@ -64,28 +85,43 @@ int main(int args, char ** argv){
 	std::string jsonfile_name,code_name;
 #pragma omp critical
 	{
-	  code_count++;
-	  if (code_count % 1000 == 0) { 
-	    //printing on nodes are very slow
-	    printf("%i/%i codes counted, %i mistakes.\n",code_count,code_total, code_mistake);
-	  }
-	  std::getline(file, jsonfile_name);
+	code_count++;
+	if (code_count % 1000 == 0){
+	  //printing on nodes are very slow
+	  printf("%i/%i codes counted, %i mistakes.\n",code_count,code_total, code_mistake);
+	  std::cout<<"Gx density:"<<rho_x_total /n_Gx_total
+	     <<",\tGz density:"<<rho_z_total /n_Gz_total
+	     <<std::endl;
 	}
+	std::getline(file, jsonfile_name);
+      }//critical
 	code_name = jsonfile_name.substr(0, jsonfile_name.size()-5);
 	//        printf("%s", line.c_str());
 	//	printf("%s", code_name.c_str());
-	if (verify(code_folder+code_name)){
+	//	if (verify(code_folder+code_name)){
+	double rho_x,rho_z;
+	int n_Gx,n_Gz;
+	if (verify(code_folder+code_name, rho_x, rho_z, n_Gx, n_Gz)){
 	  //	  std::cout<<"equal"<<std::endl;
 	}else{
 	  code_mistake++;
 	  //  std::cout<<code_name<<":\td = "<<data["d"]
 	  //	   <<", code.d = " <<code.d<<std::endl;
 	}
-	//	break;
-
+#pragma omp critical
+	{
+	rho_x_total += rho_x*n_Gx;
+	rho_z_total += rho_z*n_Gz;
+	n_Gx_total += n_Gx;
+	n_Gz_total += n_Gz;
+	}//critical
       }//pragma for
       file.close();
-    }//if
+    }//    if (file.is_open()) 
+
+    std::cout<<"Gx density:"<<rho_x_total /n_Gx_total
+	     <<",\tGz density:"<<rho_z_total /n_Gz_total
+	     <<std::endl;
     std::cout<<code_count<<" codes checked, "
 	     <<code_mistake<<" mistakes found"<<std::endl;
     return 0;
