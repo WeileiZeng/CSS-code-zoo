b9e61d2fdb90447a4d0a3665ca7ff695af118fc0
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2023-01-12 15:16:55 +0800

    running on debug node

--- pymatching/sbatch_toric.sh
+++ pymatching/sbatch_toric.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#SBATCH --time=1-00:00:00
+#SBATCH --job-name="toric"
+#SBATCH --time=1-02:00:00
 #SBATCH -n 1
-#SBATCH --cpus-per-task=31
+ #SBATCH -p small
+#SBATCH --cpus-per-task=32
 
 source pymatching-env/bin/activate
 
-python toric.py --num_trials=10000000 --num_errors_max=100 --pool_size=31
+date
+python -u toric.py --num_trials=100000000 --num_errors_max=100 --pool_size=32
+date
 
+#estimated runtime: 3.5 mins for single data, p,L, 1e8. 6 hours to finish all
