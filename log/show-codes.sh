17c50565243b5323161b9e2068a06f502d2fb444
Weilei Zeng at qlab01 <weileizeng@zhejianglab.com>
2023-09-22 10:56:57 +0800

    delete outdated scripts

--- log/show-codes.sh
+++
@@ -1,2 +0,0 @@
-echo "this display the last lines in log file, and one can check number of codes saved in each run."
-for i in gen*; do tail -n 3 $i |head -n 1; done
--- log/show-save.sh
+++
@@ -1,3 +0,0 @@
-echo "this count the lines with 'save', which is the same as number of codes saved during this trial"
-grep Saved gen* | wc -l
-#cat generate* |grep save |wc -l
